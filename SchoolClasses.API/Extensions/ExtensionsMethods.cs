24ccbc6721000a3448f33be4c8a1653fe43157d5
Cauê Gualberto <caue.gualberto00@gmail.com>
2024-05-19 16:29:51 -0300

    feat: inplement extensions methods in program.cs

--- SchoolClasses.API/Extensions/ExtensionsMethods.cs
+++ SchoolClasses.API/Extensions/ExtensionsMethods.cs
@@ -1,18 +1,27 @@
-﻿namespace SchoolClasses.API.Extensions
+﻿using SchoolClasses.Application.Services;
+using SchoolClasses.Core.Interfaces.Repositories;
+using SchoolClasses.Infrastructure.DataBase;
+using SchoolClasses.Infrastructure.Repositories;
+
+namespace SchoolClasses.API.Extensions
 {
     public static class ExtensionsMethods
     {
         public static void AddInterfacesDatabase(IServiceCollection service)
         {
-            
+            service.AddScoped<IDatabaseProvider, DatabaseProvider>();
         }
         public static void AddInterfacesRepositories(IServiceCollection service)
         {
-
+            service.AddScoped<IAlunoRepository, AlunoRepository>();
+            service.AddScoped<ICursoRepository, CursoRepository>();
+            service.AddScoped<ITurmaRepository, TurmaRepository>();
         }
         public static void AddInterfacesServices(IServiceCollection service)
         {
-
+            service.AddScoped<IAlunoService, AlunoService>();
+            service.AddScoped<ICursoService, CursoService>();
+            service.AddScoped<ITurmaService, TurmaService>();
         }
     }
 }
--- SchoolClasses.API/Program.cs
+++ SchoolClasses.API/Program.cs
@@ -1,3 +1,5 @@
+using SchoolClasses.API.Extensions;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +8,10 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+ExtensionsMethods.AddInterfacesDatabase(builder.Services);
+ExtensionsMethods.AddInterfacesRepositories(builder.Services);
+ExtensionsMethods.AddInterfacesServices(builder.Services);
 
 var app = builder.Build();
 
